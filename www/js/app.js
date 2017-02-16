1eeaefd7daace0279018cc20e61d753ba2725424
Kristoffer <kristoffer.s.karlsson@hotmail.com>
2017-02-16 08:58:20 +0100

    added text for the path screen

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('future_me', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,13 +22,3 @@ angular.module('starter', ['ionic'])
     }
   });
 });
-
-$rootScope.global = {
-  theme: {
-    primary_color: '#28A249', //'#4a87ee',
-    additional_color: '#248E44', //'#0c63ee',
-    text_color: '#fff',
-    logo: $localstorage.get('last_logo') || './img/Bagare2.jpg'
-  }
-}
-
